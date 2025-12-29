a85a8c9209e98e580bb597790828daed5ffa94ed
Emeric11 <ramonlomeli.c@gmail.com>
2025-12-29 13:35:50 -0600

    feat(events): add Event model and EventSeeder with sample data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Events seeder
+        $this->call([
+            EventSeeder::class,
+        ]);
     }
 }
